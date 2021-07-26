c89055a7a9f00af9dae2e50cdb0968a866a6febf
Víctor Manuel Rodríguez <49946179+RD13p@users.noreply.github.com>
2021-07-26 18:21:14 -0500

    Examen 2
    Solución para el segundo examen parcial.

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 // ## Agrega la dependencia de express ##
+var express = require("express");
 
 
-var animals = [
-   {
-     animalType: "dog",
-     pet: true,
-     fierceness: 4
-   }, {
-     pet: true,
-     fierceness: 10
-   }, {
-     animalType: "giraffe",
-     pet: false,
-     fierceness: 4
-   }, {
-     animalType: "zebra",
-     pet: false,
-     fierceness: 8
-   }, {
-     animalType: "lion",
-     pet: false,
-     fierceness: 10
-   }
- ];
+var animals = [{
+    animalType: "dog",
+    pet: true,
+    fierceness: 4
+}, {
+    pet: true,
+    fierceness: 10
+}, {
+    animalType: "giraffe",
+    pet: false,
+    fierceness: 4
+}, {
+    animalType: "zebra",
+    pet: false,
+    fierceness: 8
+}, {
+    animalType: "lion",
+    pet: false,
+    fierceness: 10
+}];
 
 
 // ## Inicializa express ##
-
+var app = express();
 
 // ## Inicializa el motor de plantillas con EJS ##
-
+app.set('view engine', 'ejs');
 
 // ## Agrega el middleware de express para que el servidor soporte json ##
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 /* ############## RUTAS ################  */
 
@@ -42,6 +42,9 @@ var animals = [
       a) title:  con el valor "All" 
       b) animals: con referencia al arreglo animals. 
 */
+app.get("/all-pets", function(req, res) {
+    res.render('pages/all-pets', { title: 'ALL', animals: animals });
+});
 
 
 /* (2)  Crea una ruta POST que: 
@@ -50,13 +53,21 @@ var animals = [
    - lo agregue al arreglo animals
 
 */
- 
+app.post("/api/addAnimal", function(req, res) {
+    var value = req.body;
+    animals.push(value);
+    res.json(value);
+});
+
 /* (3)  Crea una ruta GET que: 
    - escuche en /dog  
    - renderice la página 'pages/dog' y reciba 1 objeto con 2 propiedades: 
       a) title:  con el valor "Dog" 
       b) animals: con el valor del indice[0]
-*/ 
+*/
+app.get("/dog", function(req, res) {
+    res.render('pages/dog', { title: 'Dog', dog: animals[0] });
+});
 
 
 /* (4)  Crea una ruta GET que: 
@@ -67,10 +78,26 @@ var animals = [
    - renderice la página 'pages/any' y reciba 1 objeto con 2 propiedades: 
       a) title:  con el valor obtenido de la ruta dinámica
       b) animal: con la variable que almacena el objeto encontrado. Si no lo encuentra la variable se va vacía
-*/ 
-   
+*/
+app.get("/api/getAnimal/:animal", function(req, res) {
+    var newAnimal = req.params.animal;
+
+    var indexAnimal;
+    for (var i = 0; i < animals.length; i++) {
+        if (newAnimal === animals[i].animalType) {
+            indexAnimal = animals[i];
+            res.render('pages/any', { title: newAnimal, animal: indexAnimal });
+            break;
+        }
+    }
+    res.render('pages/any', { title: newAnimal, animal: indexAnimal });
+
+});
+
 
 
 //  Agrega el código necesario para que el servidor escuche en el puerto 5000
-
-
+var PORT = process.env.PORT || 50;
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
